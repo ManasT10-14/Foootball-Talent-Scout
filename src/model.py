e0d4a61ea1a9d46f98c45bc71867fa255e4dfdbe
Manas Tiwari <officialmanastiwari1410@gmail.com>
2025-02-17 02:34:43 +0530

    Random Forest used !

--- src/model.py
+++ src/model.py
@@ -1,12 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler,MinMaxScaler
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
-from sklearn.linear_model import ElasticNet
 from data_loader import DataLoader
-
+import numpy as np
 class Model:
     def __init__(self):
         self.dataLoader = DataLoader()
@@ -18,12 +15,12 @@ class Model:
         self.midAttackerFeatures, self.midAttackerData = self.dataLoader.mid_attacker()
         self.midDefenderFeatures, self.midDefenderData = self.dataLoader.mid_defender()
         
-        self.attackerModel = ElasticNet()
-        self.defenderModel = ElasticNet()
-        self.midfielderModel = ElasticNet()
-        self.goalkeeperModel = ElasticNet()
-        self.midAttackerModel = ElasticNet()
-        self.midDefenderModel = ElasticNet()
+        self.attackerModel = RandomForestRegressor()
+        self.defenderModel = RandomForestRegressor()
+        self.midfielderModel = RandomForestRegressor()
+        self.goalkeeperModel = RandomForestRegressor()
+        self.midAttackerModel = RandomForestRegressor()
+        self.midDefenderModel = RandomForestRegressor()
         
         self.__models = {
             "attacker":[self.attackerFeatures,self.attackerData,self.attackerModel],
@@ -45,24 +42,19 @@ class Model:
             print(f"Features shape: {features.shape}")
             print(f"Target shape: {data['potential'].shape}")
             
-            X_train,X_test,y_train,y_test = train_test_split(features,data["potential"],test_size=0.1,random_state=42)
-            
-            polyfeatures = PolynomialFeatures(degree=2,include_bias=False)
-            X_train = polyfeatures.fit_transform(X_train)    
-            X_test = polyfeatures.transform(X_test)  
-            
-            scaler = MinMaxScaler(feature_range=(0,100))
-            X_train = scaler.fit_transform(X_train)
-            X_test = scaler.transform(X_test)
+            X_train,X_test,y_train,y_test = train_test_split(features,data["potential"],test_size=0.1,random_state=42)  
             
             param_grid = {
-                "alpha":[0.0001,0.001,0.01,0.1,1,10,100],
-                "l1_ratio":[0.1,0.3,0.5,0.7,0.9,0.92,0.95,0.97,1]
+                "n_estimators": [64,100,128,150,200,300,500],
+                "max_features": [int(len(features.columns)/3),int(np.sqrt(len(features.columns))),int(np.log2(len(features.columns))+1)],
+                "bootstrap":[True,False],
+                "oob_score":[True],
+                
             }
-            model = GridSearchCV(model,param_grid,scoring={"RMSE": "neg_root_mean_squared_error", "R2": "r2"},cv=5,refit="RMSE")
+            model = GridSearchCV(model,param_grid,verbose=2,n_jobs=-1)
             
             model.fit(X_train,y_train)
-            return model.best_estimator_,scaler,X_test,y_test,polyfeatures
+            return model.best_estimator_,X_test,y_test
         
         else:
             raise ValueError("Incorrect position provided.")
--- src/predict.py
+++ src/predict.py
@@ -4,9 +4,7 @@ from joblib import load
 from data_loader import DataLoader
 
 
-attacker_scaler = load("models/attacker scaler.pkl")
 attacker_model = load("models/attacker model.pkl")
-attacker_poly_converter = load("models/attacker poly_converter.pkl")
 
 
 data = DataLoader()
@@ -22,13 +20,8 @@ if position == "attacker":
         value = float(input(f"{feature}: "))
         to_pred.append(value)
 
-
-    to_pred = attacker_poly_converter.transform([to_pred])
-    to_pred = attacker_scaler.transform(to_pred) 
     
-    to_pred = 40 + (60 / (1 + np.exp(-0.08 * (to_pred - 90)))) # scaling for younger player help from ChatGPT
-
-    predicted_potential = attacker_model.predict(to_pred)[0]
+    predicted_potential = attacker_model.predict([to_pred])[0]
 
 
     print(f"Predicted Potential: {predicted_potential:.2f}")
